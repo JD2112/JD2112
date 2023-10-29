0003a8ef1b631a74b82265a3caf9ffc280235c03
JD2112 <jyotirmoy21@gmail.com>
2023-10-29 09:28:49 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.14ee0c1174a9bd3fa0702c3aaa55a1eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ed499a7d48187201c31538b0338291",
+    "url": "/JD2112/index.html"
+  },
+  {
+    "revision": "ad609c460dadaba47516",
+    "url": "/JD2112/static/css/main.c354f8b1.chunk.css"
+  },
+  {
+    "revision": "053903e15902a48f09ff",
+    "url": "/JD2112/static/js/2.c166883d.chunk.js"
+  },
+  {
+    "revision": "ad609c460dadaba47516",
+    "url": "/JD2112/static/js/main.a7432c41.chunk.js"
+  },
+  {
+    "revision": "0802f8e70d11ce67ac43",
+    "url": "/JD2112/static/js/runtime~main.fd99967b.js"
+  }
+]);
